e38f8c04b8b05fe5ea762a4d6c1b776b6fb1774d
Vicky Haub <vicky.haub@gmail.com>
2017-03-07 12:00:49 +0100

    auth added

--- tripPlanClient/src/app/app.module.ts
+++ tripPlanClient/src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+import { RouterModule } from "@angular/router";
+import { routes } from './app.routing';
+
 import { AppComponent } from './app.component';
+import { SessionService } from './session.service';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [SessionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ tripPlanClient/src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { SessionService } from './session.service';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '**', redirectTo: '' }
+];

ae5ed4e41b2fef195fad16194797e025a9674813
Amanda Honkanen <amanda.honkanen@me.com>
2017-03-11 14:05:19 +0100

    small fixes in syntax

--- tripPlanClient/src/app/app.module.ts
+++ tripPlanClient/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EditComponent } from './edit/edit.component';
-// import { FileSelectDirective } from "ng2-file-upload";
+import { FileSelectDirective } from "ng2-file-upload";
 import { UserlistComponent } from './userlist/userlist.component';
 import { SearchPipe } from './pipes/search.pipe';
 import { AvatarComponent } from './avatar/avatar.component';
@@ -32,7 +32,7 @@ import { AvatarComponent } from './avatar/avatar.component';
     NavbarComponent,
     ProfileComponent,
     EditComponent,
-    // FileSelectDirective,
+    FileSelectDirective,
     UserlistComponent,
     SearchPipe,
     AvatarComponent
